dc78ee1ae32fedec1b09b4090d4a414ba786f949
Tomiwa Famuti <famutitomiwa@gmail.com>
2025-10-08 10:29:54 +0100

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./Components/Pages/home";
-import About from "./Components/Pages/About";
-import Contact from "./Components/Pages/Contact";
 import Movie from "./Components/Movie";
 import TruthButton from "./Components/TruthButton";
+import MovieCard from "./Components/MovieCard";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about-us" element={<About />}></Route>
-          <Route path="/contact-us" element={<Contact />}></Route>
-        </Routes>
-      </BrowserRouter> */}
+      <MovieCard />
 
       <Movie />
       <TruthButton />
